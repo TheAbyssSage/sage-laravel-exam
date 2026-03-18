f0071ba17846655d731f7d4a4d5d17e62d7fd8c9
TheAbyssSage <sage.stockmans@pm.me>
2026-03-18 11:36:53 +0100

    Routes for the two pages and course creation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CourseController::class, 'index']);
+
+Route::get('/courses', [CourseController::class, 'index']);
+
+Route::get('/courses/create', [CourseController::class, 'create']);
+
+Route::post('/courses', [CourseController::class, 'store']);
+
+Route::post('/courses/{id}/toggle', [CourseController::class, 'toggle']);
