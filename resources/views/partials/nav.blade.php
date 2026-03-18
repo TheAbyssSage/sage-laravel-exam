0b46f1d783032b1e19faea2f8b814bfc8af09add
TheAbyssSage <sage.stockmans@pm.me>
2026-03-18 11:32:10 +0100

    Updated navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,12 +1,12 @@
 <nav>
     <ul class="nav nav-tabs">
         <li class="nav-item">
-            <a class="nav-link {{ request()->routeIs('courses.index') ? 'active' : '' }}" 
-               href="{{ route('courses.index') }}">Courses</a>
+            <a class="nav-link {{ request()->is('courses') ? 'active' : '' }}" 
+               href="/courses">Courses</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link {{ request()->routeIs('courses.create') ? 'active' : '' }}" 
-               href="{{ route('courses.create') }}">New Course</a>
+            <a class="nav-link {{ request()->is('courses/create') ? 'active' : '' }}" 
+               href="/courses/create">New Course</a>
         </li>
     </ul>
 </nav>
